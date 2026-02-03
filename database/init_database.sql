912776b1eab4bcef78a15d609acc83dcf629d49b
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-02-03 09:21:05 -0300

    Update init_database.sql

--- database/init_database.sql
+++ database/init_database.sql
@@ -1,6 +1,17 @@
+DROP DATABASE IF EXISTS testing (FORCE);
+
 CREATE DATABASE testing;
 
 \c testing;
 
-CREATE TABLE users(id INT PRIMARY KEY, username TEXT, email TEXT, 
-			password TEXT, nationality TEXT);
+CREATE SCHEMA placeholder;
+
+CREATE TABLE placeholder.users (
+	GENERATED ALWAYS AS IDENTITY id INT PRIMARY KEY, 
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	date_of_birth DATE,
+	UNIQUE username TEXT, 
+	UNIQUE email TEXT, 
+	nationality VARCHAR[2]
+	);
