339a24d36111c66a06f69f751dbe8f8c617f3f72
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-02-11 15:25:14 -0300

    Update UserController.cs

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+      
+    }
+}
