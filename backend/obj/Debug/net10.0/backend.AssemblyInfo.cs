e39773f39875e8147a06c26e9b912a984d4f04ee
Rafael <rafaborgesdasilva75@gmail.com>
2026-02-10 21:27:44 -0300

    removed MVC backend structure

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e832834be5f5e1c763d4cef6c1aca075aa6e7ed6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97cd3150cc524b0bbc44ade611d7b5f19dc4f76d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
