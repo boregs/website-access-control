e7012747e1e73615ee5921b31502e155cdc67d1a
Rafael <rafaborgesdasilva75@gmail.com>
2026-02-11 22:40:24 -0300

    added GET and POST methods

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using backend.Models;
+using backend.Data;
 
 namespace backend.Controllers
 {
@@ -8,6 +9,28 @@ namespace backend.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-      
+
+        private readonly DAO _dao;
+        public UserController(DAO dao)
+        {
+            _dao = dao;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = _dao.Users.ToList();
+            return Ok("All users retrieved successfully.");
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(UserModel user)
+        {
+            _dao.Users.Add(user);
+            _dao.SaveChanges();
+            return Ok("User created successfully." + user.Username);
+        }
     }
+
+    
 }
