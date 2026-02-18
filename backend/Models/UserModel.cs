8334e9b313521c376ae9e65eb1d8ef8e9fae058c
Rafael <rafaborgesdasilva75@gmail.com>
2026-02-18 07:42:01 -0300

    added ID

--- backend/Models/UserModel.cs
+++ backend/Models/UserModel.cs
@@ -2,6 +2,7 @@ namespace backend.Models;
 
 public class UserModel()
 {    
+    public int Id { get; set; }
     public required string FirstName { get; set; }
 
     public required string LastName { get; set; }
