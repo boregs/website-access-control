facc9468918de713f90e23b6120bf062e39288d4
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-02-27 10:14:56 -0300

    Update UserModel.cs

--- backend/Models/UserModel.cs
+++ backend/Models/UserModel.cs
@@ -1,19 +1,26 @@
-namespace backend.Models;
-
-public class UserModel()
-{    
-    public int Id { get; set; }
-    public required string FirstName { get; set; }
-
-    public required string LastName { get; set; }
-
-    public DateOnly DateOfBirth { get; set; }
-
-    public required string Username { get; set; }
-
-    public required string Email { get; set; }
+namespace backend.Models
+{
+    public class UserModel()
+    {    
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
     
-    public required string Password { get; set; }
+        public required string LastName { get; set; }
+    
+        public DateOnly DateOfBirth { get; set; }
+    
+        public required string Username { get; set; }
+    
+        public required string Email { get; set; }
+        
+        public required string Password { get; set; }
+    
+        public string? Nationality { get; set; }
+    }
 
-    public string? Nationality { get; set; }
+    class UserModelDb : DbContext
+    {
+        public UserModelDb(DbContextOptions options) : base(options) { }
+        public DbSet<UserModel> UserModel { get; set; } = null!;
+    }
 }
