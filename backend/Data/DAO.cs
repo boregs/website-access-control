8a0c2aa219512f14624ee407f8d0c99fee57747a
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-02-27 10:18:12 -0300

    Refactor DAO constructor and initialize Users property
    Simplified constructor and initialized Users property.

--- backend/Data/DAO.cs
+++ backend/Data/DAO.cs
@@ -5,11 +5,7 @@ namespace backend.Data
 {
     public class DAO : DbContext
     {
-        public DAO(DbContextOptions<DAO> options) : base(options)
-        {
-            
-        }
-
-        public DbSet<UserModel> Users { get; set; }
+        public DAO(DbContextOptions<DAO> options) : base(options){ }
+        public DbSet<UserModel> Users { get; set; } = null!;
     }
 }
